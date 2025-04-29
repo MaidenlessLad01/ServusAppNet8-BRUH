1190bfb01a368a174792d7558a65c8fcc11e2d47
MaidenlessLad01 <cortesdemaestro@gmail.com>
2025-04-29 19:24:18 +0800

    Latestt

--- ServusAppNet8/MVVM/ViewModels/UserViewModel.cs
+++ ServusAppNet8/MVVM/ViewModels/UserViewModel.cs
@@ -11,6 +11,7 @@ using ServusAppNet8.Database;
 using ServusAppNet8.MVVM.Views;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
+using System.Runtime.CompilerServices;
 
 namespace ServusAppNet8.MVVM.ViewModels
 {
@@ -365,7 +366,7 @@ namespace ServusAppNet8.MVVM.ViewModels
         #endregion
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
